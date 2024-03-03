36ff2b2c0b85220989feb20270dbb0dcf8798a44
Alexander <alexanderher@vmware.com>
2024-03-03 10:27:08 +0100

    [TASK-ID] iac: remove the modules and resources that will be moved to another repositories

--- secrets.tf
+++
@@ -1,8 +0,0 @@
-resource "kubernetes_secret_v1" "secrets" {
-  depends_on = [kubernetes_namespace_v1.namespace]
-  metadata {
-    name      = "${var.name}-secrets"
-    namespace = var.namespace
-  }
-  data = local.secret_env
-}
